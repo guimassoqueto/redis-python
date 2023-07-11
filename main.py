3ff6bc5671360ada3a208d410447f91011777c61
guimassoqueto <gui.massoqueto@gmail.com>
2023-07-11 09:03:35 -0300

    feat: basic redis setter

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("ok")
+import redis
+from json import dumps, loads
+
+r = redis.Redis(host='localhost', port=6379, db=0)
+
+some_list = ["lula", "bolsonaro"]
+
+r.set('amazon_pids', dumps(some_list)) # dumps: convert array to string 
+
+# pids = loads(r.get('amazon_pids'))
+# print(pids)
+#r.delete('amazon_pids')
+
